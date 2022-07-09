0171ce5f0012153a4fed5b634f23866fefc490e7
Tim Viney <t.c.viney@gmail.com>
2022-07-09 18:31:28 +0100

    Adding solution 234

---
+++ 234-PalindromeLinkedList/Solution234.cs
@@ -0,0 +1,64 @@
+﻿namespace _234_PalindromeLinkedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution234
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head.next == null) return true;
+
+            ListNode middle = FindMiddle(head);
+
+            ListNode last = ReverseAndReturnLast(middle);
+
+            while (last.next != null)
+            {
+                if (last.val != head.val) return false;
+                last = last.next;
+                head = head.next;
+            }
+            return true;
+        }
+
+        private static ListNode ReverseAndReturnLast(ListNode middle)
+        {
+            ListNode next = middle.next;
+            ListNode previous = middle;
+            middle.next = null;
+            while (next.next != null)
+            {
+                ListNode oldNext = next.next;
+                next.next = previous;
+                previous = next;
+                next = oldNext;
+            }
+            next.next = previous;
+
+            return next;
+        }
+
+        private static ListNode FindMiddle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+    }
+}
--- Leetcode-solutions/Program.cs
+++ Leetcode-solutions/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var roman = new _12_IntegerToRoman.Solution12();
-Console.WriteLine(roman.IntToRoman(40));
+//var roman = new _12_IntegerToRoman.Solution12();
+//Console.WriteLine(roman.IntToRoman(40));
+using _234_PalindromeLinkedList;
+
+ListNode end = new ListNode(2);
+ListNode start = new ListNode(1,end);
+
+Solution234 s = new Solution234();
+s.IsPalindrome(start);
