a8bf9e43da0227fc95ab650bfacf5eea54935a29
Tim Viney <t.c.viney@gmail.com>
2022-07-17 16:23:44 +0100

    adding sol 876

---
+++ 876-MiddleOfLinkedList/Solution876.cs
@@ -0,0 +1,38 @@
+﻿namespace _876_MiddleOfLinkedList
+{
+    public class Solution876
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public class Solution
+        {
+            public ListNode MiddleNode(ListNode head)
+            {
+
+
+                if (head.next == null) return head;
+
+                ListNode slow = head;
+                ListNode fast = head;
+
+                while (fast.next != null && fast.next.next != null)
+                {
+                    slow = slow.next;
+                    fast = fast.next.next;
+                }
+
+
+                if (fast.next == null) return slow;
+                return slow.next;
+            }
+        }
+    }
+}
