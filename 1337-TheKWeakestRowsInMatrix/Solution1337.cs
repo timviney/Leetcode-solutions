5966dce3c92efaf6e82163967f51aac42ddd3bc6
Tim Viney <t.c.viney@gmail.com>
2022-07-17 16:42:21 +0100

    Adding sol 1337

---
+++ 1337-TheKWeakestRowsInMatrix/Solution1337.cs
@@ -0,0 +1,43 @@
+﻿namespace _1337_TheKWeakestRowsInMatrix
+{
+    public class Solution1337
+    {
+        public int[] KWeakestRows(int[][] mat, int k)
+        {
+
+            var dict = new SortedDictionary<int, List<int>>();
+
+            for (int row = 0; row < mat.Length; row++)
+            {
+
+                int[] rowMat = mat[row];
+                int score = 0;
+                for (int pos = 0; pos < rowMat.Length; pos++)
+                {
+                    if (rowMat[pos] == 0) break;
+                    score++;
+                }
+
+                if (!dict.ContainsKey(score)) dict[score] = new List<int>() { row };
+                else dict[score].Add(row);
+            }
+
+            int count = 0;
+            var output = new int[k];
+
+            foreach (int key in dict.Keys)
+            {
+                List<int> rows = dict[key];
+
+                foreach (int row in rows)
+                {
+                    output[count] = row;
+                    count++;
+                    if (count == k) return output;
+                }
+            }
+
+            return output;
+        }
+    }
+}
