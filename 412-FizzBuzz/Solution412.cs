ecee917ad99ad72586845e9ee7f5acf35a5b2943
Tim Viney <t.c.viney@gmail.com>
2022-07-17 15:26:52 +0100

    adding solution 412

---
+++ 412-FizzBuzz/Solution412.cs
@@ -0,0 +1,28 @@
+﻿namespace _412_FizzBuzz
+{
+    public class Solution412
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+
+            var r = new List<string>(n);
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                r.Add(Get(i));
+            }
+
+            return r;
+        }
+
+        public string Get(int n)
+        {
+
+            if (n % 15 == 0) return "FizzBuzz";
+            if (n % 3 == 0) return "Fizz";
+            if (n % 5 == 0) return "Buzz";
+
+            return n.ToString();
+        }
+    }
+}
