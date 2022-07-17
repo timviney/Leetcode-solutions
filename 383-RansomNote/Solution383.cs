394c85999ec9d8a59b27fde1506a81a0b25a7b12
Tim Viney <t.c.viney@gmail.com>
2022-07-17 15:16:29 +0100

    adding solution 383

---
+++ 383-RansomNote/Solution383.cs
@@ -0,0 +1,33 @@
+﻿namespace _383_RansomNote
+{
+    public class Solution383
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+
+            Dictionary<char, int> magDict = GetDict(magazine);
+            Dictionary<char, int> noteDict = GetDict(ransomNote);
+
+            foreach (char c in noteDict.Keys)
+            {
+                if (!magDict.ContainsKey(c)) return false;
+                if (magDict[c] < noteDict[c]) return false;
+            }
+
+            return true;
+        }
+
+        public Dictionary<char, int> GetDict(string words)
+        {
+            var r = new Dictionary<char, int>();
+
+            foreach (char c in words)
+            {
+                if (!r.ContainsKey(c)) r[c] = 1;
+                else r[c] += 1;
+            }
+
+            return r;
+        }
+    }
+}
