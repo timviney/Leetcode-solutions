daee4b97218f29340e8cef3b7430ec3a0b1e6380
Tim Viney <t.c.viney@gmail.com>
2025-07-10 08:54:14 +0100

    Add Largest Divisible Subset solution (368)

---
+++ 368-LargestDivisibleSubset/Solution368.cs
@@ -0,0 +1,43 @@
+﻿namespace _368_LargestDivisibleSubset;
+
+public class Solution368
+{
+    public IList<int> LargestDivisibleSubset(int[] nums) {
+        var n = nums.Length;
+        var results = new List<int>();
+
+        if (n == 0) return results;
+        
+        Array.Sort(nums);
+
+        var lengthOfSet = new int[n];
+        var previousIndexOfSet = new int[n];
+
+        for (int i = 0; i<n; i++){
+            lengthOfSet[i] = 1;
+            previousIndexOfSet[i] = -1;
+        }
+
+        int maxIndex = 0;
+
+        for (int j = 1; j<n; j++){
+            for (int i = 0; i<j; i++){
+                bool isDiv = nums[j]%nums[i]==0;
+                if (isDiv && lengthOfSet[i] >= lengthOfSet[j]){
+                    lengthOfSet[j] = lengthOfSet[i]+1;
+                    previousIndexOfSet[j]=i;
+                }
+            }
+            if (lengthOfSet[j] > lengthOfSet[maxIndex]){
+                maxIndex = j;
+            }
+        }
+
+        while (maxIndex > -1){
+            results.Add(nums[maxIndex]);
+            maxIndex = previousIndexOfSet[maxIndex];
+        }
+
+        return results;
+    }
+}
